a55b55efc8cc8345f95fadd40c7714fb9606ae94
Finn Luxton <finlayluxton@gmail.com>
2025-10-24 22:32:17 +1300

    Texture mapping for logo
    Texture mapping for logo

--- Game1.cs
+++ Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Security.Principal;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGameLibrary;
@@ -46,17 +47,58 @@ public class Game1 : Core
 
         base.Update(gameTime);
     }
-
+    
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.Silver);
+        // Clear the back buffer.
+        GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // TODO: Add your drawing code here
+        // The bounds of the icon within the texture.
+        Rectangle iconSourceRect = new Rectangle(0, 0, 128, 128);
 
-        SpriteBatch.Begin();
-        SpriteBatch.Draw(_logo, Vector2.Zero, Color.White);
+        // The bounds of the word mark within the texture.
+        Rectangle wordmarkSourceRect = new Rectangle(150, 34, 458, 58);
+
+        // Begin the sprite batch to prepare for rendering.
+        SpriteBatch.Begin(sortMode: SpriteSortMode.FrontToBack);
+
+        // Draw only the icon portion of the texture.
+        SpriteBatch.Draw(
+            _logo,              // texture
+            new Vector2(        // position
+                Window.ClientBounds.Width,
+                Window.ClientBounds.Height) * 0.5f,
+            iconSourceRect,     // sourceRectangle
+            Color.White,        // color
+            0.0f,               // rotation
+            new Vector2(        // origin
+                iconSourceRect.Width,
+                iconSourceRect.Height) * 0.5f,
+            1.0f,               // scale
+            SpriteEffects.None, // effects
+            1.0f                // layerDepth
+        );
+
+        // Draw only the word mark portion of the texture.
+        SpriteBatch.Draw(
+            _logo,              // texture
+            new Vector2(        // position
+                Window.ClientBounds.Width,
+                Window.ClientBounds.Height) * 0.5f,
+            wordmarkSourceRect, // sourceRectangle
+            Color.White,        // color
+            0.0f,               // rotation
+            new Vector2(        // origin
+                wordmarkSourceRect.Width,
+                wordmarkSourceRect.Height) * 0.5f,
+            1.0f,               // scale
+            SpriteEffects.None, // effects
+            0.0f                // layerDepth
+        );
+
+        // Always end the sprite batch when finished.
         SpriteBatch.End();
-        
+
         base.Draw(gameTime);
     }
 }
