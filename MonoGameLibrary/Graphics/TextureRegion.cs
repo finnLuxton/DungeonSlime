78f969f1a96b1b92e1e851bd87eee7f7ce322b12
Finn Luxton <finlayluxton@gmail.com>
2025-12-02 14:36:45 +1300

    Constructors
    Constructors

--- MonoGameLibrary/Graphics/TextureRegion.cs
+++ MonoGameLibrary/Graphics/TextureRegion.cs
@@ -6,5 +6,20 @@ namespace MonoGameLibrary.Graphics;
 public class TextureRegion
 {
     
+    public Texture2D Texture { get; set; }
+    public Rectangle SourceRectangle { get; set; }
+    public int Width => SourceRectangle.Width;
+    public int Height => SourceRectangle.Height;
+
+    public TextureRegion()
+    {
+        
+    }
+
+    public TextureRegion(Texture2D texture, int x, int y, int width, int height)
+    {
+        Texture = texture;
+        SourceRectangle = new Rectangle(x, y, width, height);
+    }
     
 }
